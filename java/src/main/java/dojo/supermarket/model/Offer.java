519f953215886727ef3eea75b0bc1f7f725afc12
Bardia Eghbali <bardia.eghbali@gmail.com>
2021-01-09 20:00:23 +0330

    Move discount creation logic from ShoppingCart to Offer

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -15,4 +15,37 @@ public class Offer {
         return this.product;
     }
 
+    Discount apply(double quantity, double unitPrice) {
+        int quantityAsInt = (int) quantity;
+        Discount discount = null;
+        int x = 1;
+        if (this.offerType == SpecialOfferType.ThreeForTwo) {
+            x = 3;
+        } else if (this.offerType == SpecialOfferType.TwoForAmount) {
+            x = 2;
+            if (quantityAsInt >= 2) {
+                double total = this.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
+                double discountN = unitPrice * quantity - total;
+                discount = new Discount(product, "2 for " + this.argument, -discountN);
+            }
+
+        } else if (this.offerType == SpecialOfferType.FiveForAmount) {
+            x = 5;
+        }
+
+        int numberOfXs = quantityAsInt / x;
+        if (this.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+            discount = new Discount(product, "3 for 2", -discountAmount);
+        }
+        if (this.offerType == SpecialOfferType.TenPercentDiscount) {
+            discount = new Discount(product, this.argument + "% off", -quantity * unitPrice * this.argument / 100.0);
+        }
+        if (this.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            double discountTotal = unitPrice * quantity - (this.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+            discount = new Discount(product, x + " for " + this.argument, -discountTotal);
+        }
+        return discount;
+    }
+
 }
